b04c9239be28a93fc182958d643674126af3221f
tmdghlrla <tmdghlrla@google.com>
2023-11-27 10:50:48 +0900

    Update app_prevention.py
    remove radio-> add tabs

--- app_prevention.py
+++ app_prevention.py
@@ -3,9 +3,9 @@ import streamlit as st
 def app_prevention_run() :    
     
     transportation = ['운전자','보행자']
-    selected_view = st.selectbox('예방법', transportation)
+    selected_view = st.tabs(transportation)
 
-    if selected_view == transportation[0] :
+    with selected_view[0] :
         st.text('출처:전라북도교육청')
         st.image('https://news.jbe.go.kr/upload_data/board_data/BBS_0000191/162365714867017.jpg', width=600)
         st.text('')
@@ -13,8 +13,6 @@ def app_prevention_run() :
         st.text('출처:도로교통공단')
         st.image('https://www.safetimes.co.kr/news/photo/202203/109177_92301_647.png', width=600)
         
-    elif selected_view == transportation[1] :
+    with selected_view[1] :
         st.text('출처 : 한국교통안전공단')
         st.image('https://savethelife.kr/wp-content/uploads/2020/09/%EB%B3%B4%ED%96%89_800-1000-1-1.png', width=600)
-    else : 
-        st.text('')
