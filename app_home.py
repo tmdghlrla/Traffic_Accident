3a3baa32ace46bdac47fc1e7ecafe449645be180
tmdghlrla <tmdghlrla@google.com>
2023-11-27 17:33:30 +0900

    Update app_home.py
    remove home menu image

--- app_home.py
+++ app_home.py
@@ -4,8 +4,9 @@ import PIL.Image as image
 from urllib import request
 
 def app_home_run() :        
-    st.image('https://i.imgur.com/dJI9hii.jpg', width=700)
     st.text('2005년부터 2022년까지 시도별 교통사고 데이터입니다.')
+    # st.image('https://t3.ftcdn.net/jpg/02/28/23/16/240_F_228231632_Tsn7JJnIuDBUtlau852jRThmloZN1J7L.jpg', width=1000)
+    
 
     # st.markdown(
     #      f"""
